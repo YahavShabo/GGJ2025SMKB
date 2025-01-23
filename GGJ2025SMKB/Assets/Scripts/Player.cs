44a82c155144629f707db7920dc3f45be11ca1e1
YahavShabo <yahavshabo@gmail.com>
2025-01-23 14:33:15 +0200

    Player

--- GGJ2025SMKB/Assets/Scripts/Player.cs
+++ GGJ2025SMKB/Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using Mono.Cecil;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEditor.U2D.Path.GUIFramework;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,16 +10,22 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour, GameControls.IControlsActions
 {
     GameControls controls;
-    public float defaultGS;// gravity scale
-    float moveX = 0f;
-    Vector2 aim = new Vector2 (0,0);
-    float currentGS;
+    public float defaultGS; // Gravity scale
+    public float currentGS;
     public float moveSpeed = 4f;
-    float rotZ;
-    public Transform rot;
     public float fireRate = 0.5f;
-    float lastfire;
-    bool holdingFire = false;
+    public Transform rot;
+
+    public int angle;
+    public int moveDir = 1;
+
+    public float aimDir = 1;
+    public float add = 0;
+    private float moveX = 0f;
+    private Vector2 aim = new Vector2(0, 0);
+    private float lastfire;
+    private bool holdingFire = false;
+    private float rotZ;
     void Awake() 
     {
         controls = new GameControls();
@@ -60,12 +68,45 @@ public class Player : MonoBehaviour, GameControls.IControlsActions
     }
     public void Aim()
     {
-        if (aim != Vector2.zero)
+        // Check if the player is aiming
+        bool isAiming = aim != Vector2.zero;
+
+        if (isAiming)
         {
+            // Calculate rotation and angle
             rotZ = Mathf.Atan2(aim.y, aim.x) * Mathf.Rad2Deg;
-            rot.transform.rotation = Quaternion.Euler(0, 0, rotZ);
-            //angle = Mathf.FloorToInt((Mathf.Atan2(aim.y, aim.x) * Mathf.Rad2Deg) + 360) % 360;
-            //if needed to mirror the weapon take from GHOSTSZ
+            rot.transform.rotation = Quaternion.Euler(0, 0, rotZ + 180 * add);
+            angle = Mathf.RoundToInt((Mathf.Atan2(aim.y, aim.x) * Mathf.Rad2Deg) + 360) % 360;
+
+            // Determine direction based on angle
+            if ((angle >= 0 && angle < 90) || (angle > 270 && angle <= 360)) // Right rotation
+            {
+                add = 0;
+                aimDir = 1;
+            }
+            else if (angle > 90 && angle < 270) // Left rotation
+            {
+                add = 1;
+                aimDir = -1;
+            }
+
+            // Set object scale for aiming direction
+            transform.localScale = new Vector3(aimDir * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else
+        {
+            // Set object scale for movement direction
+            transform.localScale = new Vector3(moveDir * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+
+        // Update movement direction based on input
+        if (moveX > 0)
+        {
+            moveDir = 1;
+        }
+        else if (moveX < 0)
+        {
+            moveDir = -1;
         }
     }
 
@@ -90,4 +131,5 @@ public class Player : MonoBehaviour, GameControls.IControlsActions
             lastfire = Time.timeSinceLevelLoad;
         }
     }
+    //public void ChangeSide()
 }
